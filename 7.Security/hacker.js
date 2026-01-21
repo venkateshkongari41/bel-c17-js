08ba41d806d09267834399916d4b8cbd31339908
Utkarsh Thakur <thakurutkarsh22@gmail.com>
2026-01-21 22:10:06 +0530

    security 2

---
+++ 7.Security/hacker.js
@@ -0,0 +1,4 @@
+console.log("Hacker file loaded");
+
+
+console.log(JSON.stringify(process.env));
--- 7.Security/index.js
+++ 7.Security/index.js
@@ -19,20 +19,20 @@
  */
 
 
-const express = require('express');
-const PORT = 8089;
-const server = express();
+// const express = require('express');
+// const PORT = 8089;
+// const server = express();
 
-server.get("/api/v1/auth/hello", (req, res) => {
+// server.get("/api/v1/auth/hello", (req, res) => {
 
-    const name = req.query.name;
+//     const name = req.query.name;
 
-    res.status(200).send(`<h1 id="hello"> Hello ${name} </h1>`)
-    // 
+//     res.status(200).send(`<h1 id="hello"> Hello ${name} </h1>`)
+//     // 
 
-    // `<h1> Hello <script>console.log("aaaa")</srcipt> </h1>`
-    // `<h1> Hello <script>document.cokkies</srcipt> </h1>`
-});
+//     // `<h1> Hello <script>console.log("aaaa")</srcipt> </h1>`
+//     // `<h1> Hello <script>document.cokkies</srcipt> </h1>`
+// });
 
 // PREVENTION 
 // 1. sanatization 
@@ -53,6 +53,79 @@ server.get("/api/v1/auth/hello", (req, res) => {
 
 
 
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// server.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+// PROBLEM //  DDOS attack - distributed denial of service attack  - multiple systems attack a single system
+// -------------- Rate Limiting ------------------
+
+// const expressRateLimit = require('express-rate-limit');
+
+// const express = require('express');
+// const PORT = 8089;
+// const server = express();
+
+// const limiter = expressRateLimit({
+//     windowMs: 1 * 60 * 1000, // 1 minutes
+//     max: 3, // limit each IP to 100 requests per windowMs
+//     message: 'Too many requests from this IP, please try again after a minute',
+//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
+
+
+// server.use(limiter);
+
+
+
+
+// server.get("/api/v1/hello", (req, res) => {
+
+//     res.status(200).send(`<h1 id="hello"> Hello User </h1>`)
+    
+// });
+
+
+// server.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+
+// READ:  if you want to implement rate limiting yourself: The Token Bucket algorithm is a network traffic control method that limits data rates by analogy
+
+
+//  --------- THIRD PARTY Node js Security  ------------
+// 1. 3rd party packages vulnerabilities 
+  /**
+   * my code : 5%
+   * dependency : express - 95%
+   */
+
+  // real ATTACK: postinstall script 
+
+  // HOW TO PROTECT YOURSELF
+  /**
+   * 1. npm audit
+   * 2. use only popular packages 
+   * 3. keep your packages updated 
+   * 4. use tools like snyk 
+   */
+
+//   .npmrc file 
+//   audit-level=moderate
+//   save-exact=true
+
+
+
+//  attacks 
+/**
+ * 1. typosquatting 
+ * 2. dependency confusion 
+ * 3. postinstall script
+ * 
+ * -> https://security.snyk.io/vuln/SNYK-JS-LODASH-608086
+ * -> event stream npm attack
+ *  
+ */
